9a0047247c1800ebbaa20372175e5fab4214c637
athisii <athisiiekhe12@gmail.com>
2024-06-15 04:47:24 +0530

    change server config page

--- src/main/java/com/cdac/enrollmentstation/controller/ServerConfigController.java
+++ src/main/java/com/cdac/enrollmentstation/controller/ServerConfigController.java
@@ -14,19 +14,23 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 /**
  * @author athisii, CDAC
@@ -36,8 +40,19 @@ import java.util.stream.Collectors;
 public class ServerConfigController extends AbstractBaseController {
     private static final Logger LOGGER = ApplicationLog.getLogger(ServerConfigController.class);
     @FXML
+    private ImageView downArrowImageView;
+    @FXML
+    private ImageView upArrowImageView;
+    @FXML
+    private Label unitCaptionLabel;
+    @FXML
+    private VBox hiddenVbox;
+    @FXML
+    private HBox unitIdDropDownHBox;
+    @FXML
     private BorderPane rootBorderPane;
-    private List<Unit> units;
+    private List<Unit> units = new ArrayList<>();
+    private List<String> sortedCaptions = new ArrayList<>();
 
     @FXML
     private TextField mafisUrlTextField;
@@ -52,9 +67,6 @@ public class ServerConfigController extends AbstractBaseController {
     @FXML
     private Button fetchUnitsBtn;
 
-
-    @FXML
-    private ComboBox<String> enrollmentStationUnitIdsComboBox;
     @FXML
     private Button backBtn;
 
@@ -78,69 +90,62 @@ public class ServerConfigController extends AbstractBaseController {
     @FXML
     private void editBtnAction() {
         updateUi("");
-        enableControls(mafisUrlTextField, enrollmentStationIdTextField, enrollmentStationUnitIdsComboBox, fetchUnitsBtn);
+        if (hiddenVbox.isVisible()) {
+            hiddenVbox.getChildren().remove(0, hiddenVbox.getChildren().size());
+            hiddenVbox.setVisible(false);
+            upArrowImageView.setVisible(false);
+            downArrowImageView.setVisible(true);
+        }
+        enableControls(mafisUrlTextField, enrollmentStationIdTextField, fetchUnitsBtn);
     }
 
 
-    private void saveToFile(Unit unit) {
-        if (mafisUrlTextField.getText().isBlank()) {
-            messageLabel.setText("Invalid mafis url.");
-            return;
-        }
-        if (enrollmentStationIdTextField.getText().isBlank()) {
-            messageLabel.setText("Enrolment station id is empty.");
-            return;
-        }
-
-        PropertyFile.changePropertyValue(PropertyName.MAFIS_API_URL, mafisUrlTextField.getText());
-        PropertyFile.changePropertyValue(PropertyName.ENROLLMENT_STATION_ID, enrollmentStationIdTextField.getText());
+    private void saveUnitIdAndCaption(Unit unit) {
         PropertyFile.changePropertyValue(PropertyName.ENROLLMENT_STATION_UNIT_ID, unit.getValue());
         PropertyFile.changePropertyValue(PropertyName.ENROLLMENT_STATION_UNIT_CAPTION, unit.getCaption());
-        messageLabel.setText("Updated successfully.");
+        messageLabel.setText("Enrolment Station Unit ID updated successfully.");
     }
 
 
     @FXML
     private void fetchBtnAction() {
-        if (mafisUrlTextField.getText().isBlank()) {
-            messageLabel.setText(("Not a valid url."));
-            return;
+        if (hiddenVbox.isVisible()) {
+            hiddenVbox.getChildren().remove(0, hiddenVbox.getChildren().size());
+            hiddenVbox.setVisible(false);
+            upArrowImageView.setVisible(false);
+            downArrowImageView.setVisible(true);
         }
         homeBtn.requestFocus();
         messageLabel.setText("Fetching units...");
-        disableControls(backBtn, homeBtn, editBtn, fetchUnitsBtn);
-        enrollmentStationUnitIdsComboBox.setItems(FXCollections.observableArrayList());
-        enrollmentStationUnitIdsComboBox.setValue(null); // selected value
+        disableControls(backBtn, homeBtn, editBtn, unitIdDropDownHBox, fetchUnitsBtn);
+        sortedCaptions = new ArrayList<>();
+        units = new ArrayList<>();
         App.getThreadPool().execute(this::fetchUnits);
     }
 
     private void fetchUnits() {
         try {
-            PropertyFile.changePropertyValue(PropertyName.MAFIS_API_URL, mafisUrlTextField.getText());
             units = MafisServerApi.fetchAllUnits();
         } catch (GenericException ex) {
             updateUi(ex.getMessage());
-            enableControls(backBtn, homeBtn, editBtn, fetchUnitsBtn);
+            enableControls(backBtn, homeBtn, editBtn, unitIdDropDownHBox, fetchUnitsBtn);
             return;
         } catch (ConnectionTimeoutException ex) {
             Platform.runLater(() -> {
                 messageLabel.setText("Connection timeout. Please try again.");
-                enableControls(backBtn, homeBtn, editBtn, fetchUnitsBtn);
+                enableControls(backBtn, homeBtn, editBtn, unitIdDropDownHBox, fetchUnitsBtn);
             });
             return;
         }
 
         if (units == null || units.isEmpty()) {
             updateUi("No units for selected mafis url.");
-            enableControls(backBtn, homeBtn, editBtn, fetchUnitsBtn);
+            enableControls(backBtn, homeBtn, editBtn, unitIdDropDownHBox, fetchUnitsBtn);
             return;
         }
-        List<String> captions = units.stream().map(Unit::getCaption).collect(Collectors.toList());
-        Platform.runLater(() -> enrollmentStationUnitIdsComboBox.setItems(FXCollections.observableArrayList(captions)));
-        String enrollmentStationUnitCaption = PropertyFile.getProperty(PropertyName.ENROLLMENT_STATION_UNIT_CAPTION);
-        Platform.runLater(() -> enrollmentStationUnitIdsComboBox.getSelectionModel().select(enrollmentStationUnitCaption));
+        sortedCaptions = units.stream().map(Unit::getCaption).sorted().toList();
         updateUi("Units fetched successfully.");
-        enableControls(backBtn, homeBtn, editBtn, fetchUnitsBtn);
+        enableControls(backBtn, homeBtn, editBtn, unitIdDropDownHBox, fetchUnitsBtn);
     }
 
     // calls automatically by JavaFX runtime
@@ -176,15 +181,89 @@ public class ServerConfigController extends AbstractBaseController {
 
         mafisUrlTextField.setText(mafisUrl);
         enrollmentStationIdTextField.setText(enrollmentStationId);
-        enrollmentStationUnitIdsComboBox.getSelectionModel().select(enrollmentStationUnitCaption);
-        enrollmentStationUnitIdsComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            // sometimes old and new value will be null.
-            if (newValue != null) {
-                Optional<Unit> unitOptional = units.stream().filter(u -> u.getCaption().equals(newValue)).findFirst();
-                unitOptional.ifPresent(this::saveToFile);
-            }
-        });
+        unitCaptionLabel.setText(enrollmentStationUnitCaption);
 
+        enrollmentStationIdTextField.textProperty().addListener((observable, oldValue, newValue) -> saveEnrollmentStationId(newValue));
+        mafisUrlTextField.textProperty().addListener((observable, oldValue, newValue) -> saveMafisUrl(newValue));
+
+        unitIdDropDownHBox.setOnMouseClicked(this::toggleUnitCaptionListView);
+    }
+
+    private void saveMafisUrl(String newValue) {
+        if (newValue != null && !newValue.isBlank()) {
+            PropertyFile.changePropertyValue(PropertyName.MAFIS_API_URL, newValue);
+            enableControls(backBtn, editBtn, enrollmentStationIdTextField, unitIdDropDownHBox, homeBtn, fetchUnitsBtn);
+            if (hiddenVbox.isVisible()) {
+                hiddenVbox.setDisable(false);
+            }
+            updateUi("Mafis API Server Url updated successfully");
+        } else {
+            updateUi("Enter a valid Mafis API Server Url");
+            disableControls(backBtn, homeBtn, editBtn, enrollmentStationIdTextField, unitIdDropDownHBox, fetchUnitsBtn);
+            if (hiddenVbox.isVisible()) {
+                hiddenVbox.setDisable(true);
+            }
+        }
+    }
+
+    private void saveEnrollmentStationId(String newValue) {
+        if (newValue != null && !newValue.isBlank()) {
+            PropertyFile.changePropertyValue(PropertyName.ENROLLMENT_STATION_ID, newValue);
+            enableControls(backBtn, editBtn, mafisUrlTextField, unitIdDropDownHBox, homeBtn, fetchUnitsBtn);
+            if (hiddenVbox.isVisible()) {
+                hiddenVbox.setDisable(false);
+            }
+            updateUi("Enrolment Station ID updated successfully");
+        } else {
+            updateUi("Enter a valid Enrolment Station ID");
+            disableControls(backBtn, homeBtn, editBtn, mafisUrlTextField, unitIdDropDownHBox, fetchUnitsBtn);
+            if (hiddenVbox.isVisible()) {
+                hiddenVbox.setDisable(true);
+            }
+        }
+    }
+
+    private void toggleUnitCaptionListView(MouseEvent mouseEvent) {
+        if (sortedCaptions.isEmpty() || units.isEmpty()) {
+            return;
+        }
+        if (hiddenVbox.isVisible()) {
+            hiddenVbox.getChildren().remove(0, hiddenVbox.getChildren().size());
+            hiddenVbox.setVisible(false);
+            upArrowImageView.setVisible(false);
+            downArrowImageView.setVisible(true);
+        } else {
+            hiddenVbox.setVisible(true);
+            downArrowImageView.setVisible(false);
+            upArrowImageView.setVisible(true);
+            TextField sarchTextField = new TextField();
+            sarchTextField.setPromptText("Search");
+            hiddenVbox.getChildren().add(sarchTextField);
+            ListView<String> listView = new ListView<>();
+            listView.setItems(FXCollections.observableArrayList(sortedCaptions));
+            listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                if (newValue != null) {
+                    Optional<Unit> unitOptional = units.stream().filter(u -> u.getCaption().equals(newValue)).findFirst();
+                    unitOptional.ifPresent(this::saveUnitIdAndCaption);
+                    unitCaptionLabel.setText(newValue);
+                    hiddenVbox.getChildren().remove(0, hiddenVbox.getChildren().size());
+                    hiddenVbox.setVisible(false);
+                    downArrowImageView.setVisible(true);
+                    upArrowImageView.setVisible(false);
+                }
+            });
+            sarchTextField.textProperty().addListener((observable, oldVal, newVal) -> searchFilter(newVal, listView));
+            hiddenVbox.getChildren().add(1, listView);
+        }
+    }
+
+    private void searchFilter(String value, ListView<String> listView) {
+        if (value.isEmpty()) {
+            listView.setItems(FXCollections.observableList(units.stream().map(Unit::getCaption).toList()));
+            return;
+        }
+        String valueUpper = value.toUpperCase();
+        listView.setItems(FXCollections.observableList(units.stream().map(Unit::getCaption).filter(caption -> caption.toUpperCase().contains(valueUpper)).toList()));
     }
 
     private void updateUi(String message) {
